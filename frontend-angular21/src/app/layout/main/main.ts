d60add1c7ec5da975f380f29eec8c0cc5098715b
Alfred056 <alfredovilca374@gmail.com>
2026-01-04 13:23:31 -0500

    cambios

--- frontend-angular21/src/app/layout/main/main.ts
+++ frontend-angular21/src/app/layout/main/main.ts
@@ -3,10 +3,11 @@ import { Sidebar } from "../sidebar/sidebar";
 import { Header } from "../header/header";
 import { RouterModule } from "@angular/router";
 import { ThemeService } from '../../core/services/theme.service';
+import { Dashboard } from "../../dashboard/pages/dashboard/dashboard";
 
 @Component({
   selector: 'app-main',
-  imports: [Sidebar, Header, RouterModule],
+  imports: [Sidebar, Header, RouterModule, Dashboard],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
