0efcf66e29a0ecc8da7fd89fc7c97968afdb9199
Leonardo474-cpu <leo8minaya@gmail.com>
2026-01-08 23:39:40 -0500

    Creacion de la pantalla de sede

--- frontend-angular21/src/app/app.routes.ts
+++ frontend-angular21/src/app/app.routes.ts
@@ -11,7 +11,31 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', loadComponent: () => import('./dashboard/pages/dashboard/dashboard').then(m => m.Dashboard) },
-      { path: 'administracion', loadComponent: () => import('./administracion/pages/administracion/administracion').then(m => m.Administracion) },
+      { path: 'administracion',
+        children: [
+          {
+            path: '',
+            loadComponent: () => import('./administracion/pages/administracion/administracion').then(m => m.Administracion)
+          },
+          {
+            path: 'sedes',
+            children: [
+              {
+                path: '',
+                loadComponent: () => import('./administracion/pages/sedes/pages/sedes/sedes').then(m => m.Sedes)
+              },
+              {
+                path: 'agregar-sede',
+                loadComponent: () => import('./administracion/pages/sedes/pages/agregar-sede/agregar-sede').then(m => m.AgregarSede)
+              },
+              {
+                path: 'editar-sede',
+                loadComponent: () => import('./administracion/pages/sedes/pages/editar-sede/editar-sede').then(m => m.EditarSede)
+              }
+            ]
+          },
+        ]
+      },
       { path: 'almacen', loadComponent: () => import('./almacen/pages/almacen/almacen').then(m => m.Almacen) },
       { path: 'ventas', loadComponent: () => import('./ventas/pages/ventas/ventas').then(m => m.Ventas) },
     ]
