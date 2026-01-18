3960b30dac3fdba115109d5b03f259539c2be864
AlexiVelasquez <143229121+AlexiVelasquez@users.noreply.github.com>
2026-01-18 17:25:07 -0500

    arreglado

--- frontend-angular21/src/app/administracion/pages/reportes/pages/reporte-inventario/reporte-inventario.ts
+++ frontend-angular21/src/app/administracion/pages/reportes/pages/reporte-inventario/reporte-inventario.ts
@@ -28,84 +28,30 @@ export class ReporteInventario {
 
   constructor(private messageService: MessageService) {}
 
-  // 🔹 Stock disponible (puede venir luego del backend)
-  stockDisponible: number = 60;
-
-  // 🔹 Cantidad a transferir (input)
-  cantidadTransferir: number = 1;
-
   sedes = [
     { label: 'Flores 15 - San Juan Lurigancho', value: 'flores-15' },
-    { label: 'Lurin', value: 'lurin' }
+    { label: 'Lurin', value: 'lurin' },
   ];
 
   motivos = [
-    { label: 'Reposición', value: 'reposicion' },
+    { label: 'Reposicion', value: 'reposicion' },
     { label: 'Ajuste de stock', value: 'ajuste' },
-    { label: 'Solicitud interna', value: 'solicitud' }
+    { label: 'Solicitud interna', value: 'solicitud' },
   ];
 
   responsables = [
-    { label: 'Jefatura de almacén', value: 'jefatura' },
-    { label: 'Supervisor de sede', value: 'supervisor' }
+    { label: 'Jefatura de almacen', value: 'jefatura' },
+    { label: 'Supervisor de sede', value: 'supervisor' },
   ];
 
-  // ➕ Botón +
-  aumentarCantidad() {
-    if (this.cantidadTransferir < this.stockDisponible) {
-      this.cantidadTransferir++;
-    }
-  }
-
-  // ➖ Botón -
-  disminuirCantidad() {
-    if (this.cantidadTransferir > 1) {
-      this.cantidadTransferir--;
-    }
-  }
-
-  // ✏️ Escritura manual (doble click / teclado)
-  validarCantidadManual() {
-    if (!this.cantidadTransferir || this.cantidadTransferir < 1) {
-      this.cantidadTransferir = 1;
-    }
-
-    if (this.cantidadTransferir > this.stockDisponible) {
-      this.cantidadTransferir = this.stockDisponible;
-    }
-  }
-
   confirmarTransferencia() {
-
-    const nuevaTransferencia = {
-      codigo: 'TRF-' + Math.floor(1000 + Math.random() * 9000),
-      producto: 'Cable HDMI 2m',
-      origen: 'Flores 15',
-      destino: 'Lurin',
-      cantidad: this.cantidadTransferir,
-      estado: 'Pendiente',
-      fecha: new Date().toLocaleDateString()
-    };
-
-    const transferenciasGuardadas = JSON.parse(
-      localStorage.getItem('transferencias') || '[]'
-    );
-
-    transferenciasGuardadas.unshift(nuevaTransferencia);
-
-    localStorage.setItem(
-      'transferencias',
-      JSON.stringify(transferenciasGuardadas)
-    );
-
     this.messageService.add({
       severity: 'success',
       summary: 'Registro Exitoso',
-      detail: 'Se realizó la solicitud de Transferencia',
+      detail: 'Se realizó un cambio en el inventario',
       life: 3000
     });
-
-    // Reset
-    this.cantidadTransferir = 1;
   }
+
+  
 }
